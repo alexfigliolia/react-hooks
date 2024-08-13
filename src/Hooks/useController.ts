1bdce660bb4c9e8bc5fe95fc6447b2635c1e7321
Alex Figliolia <alexfigliolia@gmail.com>
2024-08-13 12:17:45 -0400

    Remove test

--- src/Hooks/useController.ts
+++ src/Hooks/useController.ts
@@ -2,9 +2,5 @@ import { useRef } from "react";
 
 export const useController = <T>(Controller: T) => {
   const controller = useRef(Controller);
-  void fetch("/api", {
-    method: "POST",
-    body: new FormData(undefined as unknown as HTMLFormElement),
-  });
   return controller.current;
 };
