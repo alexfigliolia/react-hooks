0b5c4ce915e055d4bf21ef7f64499c0e92ce34d9
Alex Figliolia <alexfigliolia@gmail.com>
2024-10-23 13:55:01 -0400

    Bugfix useLocale

--- src/Hooks/useLocale.ts
+++ src/Hooks/useLocale.ts
@@ -1,19 +1,22 @@
 import { useCallback, useEffect, useState } from "react";
 
 const getLocale = (defaultLocale: string) => {
-  window?.navigator?.language ?? defaultLocale;
+  return window?.navigator?.language ?? defaultLocale;
 };
 
 export const useLocale = (defaultLocale = "en-us") => {
   const [locale, setLocale] = useState(getLocale(defaultLocale));
+
   const onChange = useCallback(() => {
     setLocale(getLocale(defaultLocale));
   }, [defaultLocale]);
+
   useEffect(() => {
     window.addEventListener("languagechange", onChange);
     return () => {
       window.removeEventListener("languagechange", onChange);
     };
-  });
+  }, [onChange]);
+
   return locale;
 };
